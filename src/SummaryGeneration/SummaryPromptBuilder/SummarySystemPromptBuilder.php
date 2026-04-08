45a9a4eb8b24cd9205a95fe3fb896dee55db9e98
Pierre Rolland <pierre@rolland.dev>
2026-04-08 20:52:06 +0200

    Use Markdown with long text

--- src/SummaryGeneration/SummaryPromptBuilder/SummarySystemPromptBuilder.php
+++ src/SummaryGeneration/SummaryPromptBuilder/SummarySystemPromptBuilder.php
@@ -10,7 +10,8 @@ class SummarySystemPromptBuilder
 <<<PROMPT
 Summarize the subject in %d-%d sentences.
 You are allowed, and encouraged, to produce clear paragraphs to organize your ideas, separated by double line breaks.
-You are allowed to use markdown helpers to highlight some concepts.
+You are encouraged to use markdown helpers to highlight some concepts.
+If the whole text is longer than 10 sentences, you are strongly encouraged to produce a clear Markdown document, with accurate sectioning.
 The concepts you outline need to be structured and streamlined in a way a human can easily process them.
 In your sentences, reuse the names of the concepts and entities you find in the features. It is very important for ubiquitous language concerns. Your are allowed, and encouraged, to explain what they are.
 Do NOT output Gherkin or any headings or bullets.
